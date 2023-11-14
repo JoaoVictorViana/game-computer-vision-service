3242fba458aa8eb870b3dc664773337f0a761195
João Victor Viana <jv.duarte.viana@gmail.com>
2023-11-14 01:45:47 -0300

    feat: detecção de face

--- server.py
+++ server.py
@@ -2,13 +2,22 @@ from flask import Flask, request
 from werkzeug.utils import secure_filename
 from src.services.FaceDetector import FaceDetector
 from PIL import Image
+import os
 
 app = Flask(__name__)
 
 @app.route("/upload", methods=['POST'])
 def upload_images():
     f = request.files['image']
-    image = Image.open(f)
+
+    os.makedirs(os.path.join(app.root_path, 'src/storage/faces/raw'), exist_ok=True)
+    os.makedirs(os.path.join(app.root_path, 'src/storage/faces/detect'), exist_ok=True)
+
+    fileName = os.path.join(app.root_path, 'src/storage/faces/raw', secure_filename(f.filename))
+    f.save(fileName)
+
+    output = os.path.join(app.root_path, 'src/storage/faces/detect/')
+
     faceDetector = FaceDetector()
-    faceDetector.detect(image, secure_filename(f.filename))
+    faceDetector.detect(fileName, output)
     return 'success'
--- src/services/FaceDetector.py
+++ src/services/FaceDetector.py
@@ -8,17 +8,23 @@ class FaceDetector:
     __slots__ = ("classificator", "width", "height")
 
     def __init__(self) -> None:
-        self.classificator = cv2.CascadeClassifier("../models/haarcascade_frontalface_default.xml")
+        self.classificator = cv2.CascadeClassifier(filename=cv2.data.haarcascades + "haarcascade_frontalface_default.xml")
         self.width = WIDTH_DEFAULT
         self.height = HEIGHT_DEFAULT
     
-    def detect(self, image, fileName):
+    def detect(self, fileName, output):
         # image = cv2.imdecode(image, cv2.IMREAD_COLOR)
-
+        image = cv2.imread(filename=fileName)
         imageGray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
-        facesDetect = self.classificator.detectMultiScale(imageGray, scaleFactor=1.5, minSize=(100,100))
 
-        for(x, y, l, a) in facesDetect:
+        facesDetect = self.classificator.detectMultiScale(imageGray, scaleFactor=1.1, minNeighbors=5, minSize=(40, 40))
+
+        n = 0
+        for (x, y, l, a) in facesDetect:
+            img = cv2.rectangle(imageGray, (x, y), (x + l, y + a), (0, 255, 0), 4)
+            cv2.imwrite(f'./src/storage/faces/detect/teste-2.jpg', img)
+
             imageFace = cv2.resize(imageGray[y:y + a, x:x + 1], (self.width, self.height))
-            cv2.imwrite(f"../storage/faces/{fileName}", imageFace)
+            cv2.imwrite(f'./src/storage/faces/detect/teste-{++n}.jpg', imageFace)
+            
